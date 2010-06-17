e0477acfa86272363c81ef5d216512882d448b3c
hoge west <hogewest@gmail.com>
2010-06-18 03:47:15 +0900

    定義方法の修正

--- config/environment.rb
+++ config/environment.rb
@@ -39,8 +39,7 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
-
-  Time::DATE_FORMATS[:simple] = "%Y-%m-%d %H:%M:%S"
 end
 
+Time::DATE_FORMATS[:simple] = "%Y-%m-%d %H:%M:%S"
 require 'will_paginate'
